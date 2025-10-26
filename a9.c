a84246838a94f8fa4cc8bb01ec62809bb8cd2999
Feifei_Yu <fyu21186@gmail.com>
2025-10-26 14:15:27 -0700

    A9: Add files

--- a9.c
+++ a9.c
@@ -10,7 +10,6 @@
 
 #define HEADER_SIZE (sizeof(struct header))
 
-static bool first_initialized = true;
 static int free_space = 0;
 static int total_space = 0;
 static int space_limit = 0;
@@ -18,41 +17,95 @@ static void *base;
 static struct header *free_head = NULL;
 static enum algs alg = FIRST_FIT;
 
-struct header *find_first_fit(struct header *free_ptr, uint64_t size) {
+void insert_free_sorted(struct header *h);
+void coalesce(void);
+
+int find_first_fit(struct header *free_ptr, uint64_t size,
+                   struct header **res_out, struct header **prev_out) {
+  struct header *prev = NULL;
   while (free_ptr != NULL) {
-    if (free_ptr->size >= size)
-      return free_ptr;
+    if (free_ptr->size >= size) {
+      *res_out = free_ptr;
+      *prev_out = prev;
+      return 0;
+    }
+    prev = free_ptr;
     free_ptr = free_ptr->next;
   }
-  return NULL;
+  return -1;
 }
 
-struct header *find_best_fit(struct header *free_ptr, uint64_t size) {
+int find_best_fit(struct header *free_ptr, uint64_t size,
+                  struct header **res_out, struct header **prev_out) {
   struct header *best_ptr = NULL;
+  struct header *best_prev = NULL;
+  struct header *prev = NULL;
   uint64_t best_fit_size = UINT64_MAX;
   while (free_ptr != NULL) {
     if (free_ptr->size >= size && free_ptr->size < best_fit_size) {
       best_fit_size = free_ptr->size;
       best_ptr = free_ptr;
+      best_prev = prev;
     }
+    prev = free_ptr;
     free_ptr = free_ptr->next;
   }
-  return best_ptr;
+  if (best_ptr != NULL) {
+    *res_out = best_ptr;
+    *prev_out = best_prev;
+    return 0;
+  } else
+    return -1;
 }
 
-struct header *find_worst_fit(struct header *free_ptr, uint64_t size) {
+int find_worst_fit(struct header *free_ptr, uint64_t size,
+                   struct header **res_out, struct header **prev_out) {
   struct header *worst_ptr = NULL;
+  struct header *worst_prev = NULL;
+  struct header *prev = NULL;
   uint64_t worst_fit_size = 0;
   while (free_ptr != NULL) {
     if (free_ptr->size >= size && free_ptr->size > worst_fit_size) {
       worst_fit_size = free_ptr->size;
       worst_ptr = free_ptr;
+      worst_prev = prev;
     }
+    prev = free_ptr;
     free_ptr = free_ptr->next;
   }
-  return worst_ptr;
+  if (worst_ptr != NULL) {
+    *res_out = worst_ptr;
+    *prev_out = worst_prev;
+    return 0;
+  } else
+    return -1;
 }
 
+int grow_heap(struct header **h) {
+  void *old_ptr = sbrk(0);
+  if (sbrk(INCREMENT) == (void *)-1)
+    return -1;
+  total_space += INCREMENT;
+  free_space += INCREMENT;
+
+  struct header *ptr = (struct header *)old_ptr;
+  ptr->size = INCREMENT;
+  ptr->next = NULL;
+  *h = ptr;
+  return 0;
+}
+
+int select_space(struct header *h, int space, struct header **res,
+                 struct header **prev) {
+  int ans = -1;
+  if (alg == FIRST_FIT)
+    ans = find_first_fit(h, space, res, prev);
+  else if (alg == BEST_FIT)
+    ans = find_best_fit(h, space, res, prev);
+  else
+    ans = find_worst_fit(h, space, res, prev);
+  return ans;
+}
 /*
  * alloc() allocates memory from the heap. The first argument indicates the
  * size. It returns the pointer to the newly-allocated memory. It returns NULL
@@ -62,47 +115,73 @@ void *alloc(int size) {
   if (size <= 0)
     return NULL;
 
-  if (first_initialized) {
-    first_initialized = false;
-    if (sbrk(INCREMENT) == (void *)-1)
-      return NULL;
-    total_space = INCREMENT;
-    free_space = INCREMENT;
-    free_head = base;
-    free_head->next = NULL;
-  }
-
   const int need_space = HEADER_SIZE + size;
-  if (free_space < need_space && total_space + INCREMENT <= space_limit) {
-    if (sbrk(INCREMENT) == (void *)-1)
+  if (free_space < need_space) {
+    if (total_space + INCREMENT > space_limit)
       return NULL;
-    total_space += INCREMENT;
-    free_space += INCREMENT;
-  } else if (free_space < need_space && total_space + INCREMENT > space_limit)
-    return NULL;
+
+    // Increase the end_header's space
+    struct header *h = NULL;
+    int output = grow_heap(&h);
+    if (output == -1)
+      return NULL;
+
+    insert_free_sorted(h);
+    coalesce();
+  }
 
   struct header *h = free_head;
   struct header *res = NULL;
-  if (alg == FIRST_FIT)
-    // first fit algorithm
-    res = find_first_fit(h, need_space);
-  else if (alg == BEST_FIT)
-    // BEST_FIT
-    res = find_best_fit(h, need_space);
-  else
-    // WORST_FIT
-    res = find_worst_fit(h, need_space);
-
-  if (!res)
+  struct header *prev = NULL;
+  int output = select_space(h, need_space, &res, &prev);
+  if (output == -1)
     return NULL;
 
-  res->size = need_space;
-  free_space -= need_space;
-  free_head = res + need_space;
-  free_head->size = free_space;
-  free_head->next = NULL;
+  if (prev)
+    prev->next = res->next;
+  else
+    free_head = res->next;
+  res->next = NULL;
+
+  uint64_t rem = res->size - need_space;
+  if (rem < HEADER_SIZE) {
+    free_space -= res->size;
+  } else {
+    struct header *remh = (struct header *)((char *)res + need_space);
+    remh->size = rem;
+    remh->next = NULL;
+    insert_free_sorted(remh);
+    coalesce();
+    res->size = need_space;
+    free_space -= (int)need_space;
+  }
 
   return (char *)res + HEADER_SIZE;
+}
+void insert_free_sorted(struct header *h) {
+  if (!free_head || h < free_head) {
+    h->next = free_head;
+    free_head = h;
+    return;
+  }
+  struct header *cur = free_head;
+  while (cur->next && cur->next < h)
+    cur = cur->next;
+  h->next = cur->next;
+  cur->next = h;
+}
+
+void coalesce(void) {
+  struct header *cur = free_head;
+  while (cur && cur->next) {
+    char *end_cur = (char *)cur + cur->size;
+    if (end_cur == (char *)cur->next) {
+      cur->size += cur->next->size;
+      cur->next = cur->next->next;
+    } else {
+      cur = cur->next;
+    }
+  }
 }
 
 // dealloc() frees the memory pointer to by the first argument.
@@ -116,20 +195,9 @@ void dealloc(void *node) {
     return;
 
   free_space += h->size;
-  h->next = free_head;
-  free_head = h;
-
-  // coalesce
-  struct header *cur = free_head;
-  while (cur && cur->next) {
-    struct header *end_cur = cur + cur->size;
-    if (end_cur == cur->next) {
-      cur->size += end_cur->size;
-      cur->next = end_cur->next;
-    } else {
-      cur = cur->next;
-    }
-  }
+  h->next = NULL;
+  insert_free_sorted(h);
+  coalesce();
 }
 
 /*
@@ -153,14 +221,15 @@ void allocopt(enum algs option, int set_lim) {
 // allocinfo() returns the current statistics.
 struct allocinfo allocinfo(void) {
   struct allocinfo res;
-  res.free_size = (uint64_t)(free_space - HEADER_SIZE);
-
+  uint64_t size = 0;
   uint64_t largest = 0;
   uint64_t smallest = 0;
   uint64_t chunks = 0;
+
   struct header *cur = free_head;
   while (cur) {
     uint64_t s = cur->size;
+    size += s - HEADER_SIZE;
     if (s >= HEADER_SIZE) {
       chunks++;
       if (largest < s)
@@ -170,8 +239,10 @@ struct allocinfo allocinfo(void) {
     }
     cur = cur->next;
   }
+
+  res.free_size = size;
   res.free_chunks = chunks;
-  res.largest_free_chunk_size = largest;
-  res.smallest_free_chunk_size = smallest;
+  res.largest_free_chunk_size = (chunks ? largest - HEADER_SIZE : 0);
+  res.smallest_free_chunk_size = (chunks ? smallest - HEADER_SIZE : 0);
   return res;
 }
